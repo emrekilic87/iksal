4c1e9f5b20c45f506dd078984236ca71222190a5
emre kılıç <emrekilic87@gmail.com>
2021-01-17 02:03:48 +0300

    favicon and title added

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -1,6 +1,15 @@
-import '../css/style.css'
-import '../css/icomoon/style.css'
+import "../css/style.css";
+import "../css/icomoon/style.css";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+        <title>İKSAL</title> 
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
