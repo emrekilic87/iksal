b879a497f5928c561712e70390289ad45f16ba41
emre kılıç <emrekilic87@gmail.com>
2021-02-07 19:16:38 +0300

    fallback changed

--- website/pages/[post].js
+++ website/pages/[post].js
@@ -23,7 +23,7 @@ export async function getStaticPaths() {
     params: { post: post.id.toString() },
   }));
 
-  return { paths, fallback: false };
+  return { paths, fallback: true };
 }
 
 export async function getStaticProps({ params }) {
